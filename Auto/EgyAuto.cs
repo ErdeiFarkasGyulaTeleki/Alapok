442394461d24cb333fb50a0518bc5f85276f914a
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-10 10:39:23 +0200

    Auto osztály fejlesztése

---
+++ Auto/EgyAuto.cs
@@ -0,0 +1,19 @@
+﻿namespace Auto
+{
+    public class EgyAuto
+    {
+        public string Marka;
+        public int Eletkor;
+
+        public EgyAuto(string Marka, int Eletkor)
+        {
+            this.Marka = Marka;
+            this.Eletkor = Eletkor;
+        }
+
+        public void Kiiras()
+        {
+            Console.WriteLine($"Egy {Marka} márkájú autó {Eletkor} éves!");
+        }
+    }
+}
