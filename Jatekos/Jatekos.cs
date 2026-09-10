78a84edf1b2ff965d83db5f0d4315b4501d44cf3
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-10 11:19:44 +0200

    Jatekos megírása

--- Jatekos/Jatekos.cs
+++ Jatekos/Jatekos.cs
@@ -2,5 +2,19 @@
 {
     public class Jatekos
     {
+        public string Nev { get; set; }
+        public int Elet
+        {
+            get;
+            set
+            {
+                field = Math.Clamp(value, 0, 100);
+            }
+        }
+
+        public void Kiiras()
+        {
+            Console.WriteLine($"A {Nev} nevű játékos életereje: {Elet}.");
+        }
     }
 }
