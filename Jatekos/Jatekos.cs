1a8a153ade87c8098a970dc57cf40ac8fe16c697
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-10 11:25:28 +0200

    Jatekos tesztelés és kiiratás

--- Jatekos/Jatekos.cs
+++ Jatekos/Jatekos.cs
@@ -9,12 +9,21 @@
             set
             {
                 field = Math.Clamp(value, 0, 100);
+                if (log) Kiiras();
             }
         }
+
+        public bool log = false;
 
         public Jatekos(string _nev)
         {
             this.Nev = _nev;
+        }
+
+        public Jatekos(string _nev, bool _log)
+        {
+            this.Nev = _nev;
+            this.log = _log;
         }
 
         public void Kiiras()
--- Jatekos/Program.cs
+++ Jatekos/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Jatekos;
+
+Jatekos.Jatekos jatekos = new Jatekos.Jatekos("Lapos Elemér", true);
+
+jatekos.Elet = 70;
+jatekos.Elet = -10;
+jatekos.Elet = 210;
+jatekos.Elet = 69;
