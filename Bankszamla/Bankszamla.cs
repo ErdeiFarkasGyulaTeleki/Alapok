9849e31e18bc6fc3071d2b237e764593b95c637d
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-10 11:13:47 +0200

    Bankszámla változtatások

--- Bankszamla/Bankszamla.cs
+++ Bankszamla/Bankszamla.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Bankszamla
+﻿namespace Bankszamla
 {
     public class Bankszamla
     {
+        private double egyenleg;
+        private bool log;
+
+        public Bankszamla()
+        {
+            this.log = false;
+        }
+
+        public Bankszamla(bool log)
+        {
+            this.log = log;
+        }
+
+        public void Befizetés(double osszeg)
+        {
+            if (osszeg <= 0)
+            {
+                Console.WriteLine($"Nem lehet a nempozitív {osszeg} összeget befizetni!");
+                return;
+            }
+
+            egyenleg += osszeg;
+
+            if (log)
+            {
+                Console.WriteLine($"{osszeg} befizetve a számlára. Így azon {egyenleg} Forint van.");
+            }
+        }
+
+        public void Kivet(double osszeg)
+        {
+            if (osszeg > egyenleg)
+            {
+                Console.WriteLine($"Nincs annyi egyenlege, hogy {osszeg} Forint kivegyen!");
+                return;
+            }
+
+            egyenleg -= osszeg;
+
+            if (log)
+            {
+                Console.WriteLine($"{osszeg} kivéve a számláról. Így azon {egyenleg} Forint van.");
+            }
+        }
+
+        public void EgyenlegKiiras()
+        {
+            Console.WriteLine($"A számlán {egyenleg} Forint van.");
+        }
     }
 }
