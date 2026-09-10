f00280281a0e456c457ac5aba24fbabd5498cae1
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-10 11:31:04 +0200

    Konyv megoldás

--- Konyv/Konyv.cs
+++ Konyv/Konyv.cs
@@ -2,5 +2,18 @@
 {
     public class Konyv
     {
+        public string Cim;
+        public int Ar;
+
+        public Konyv(string _cim, int _ar)
+        {
+            this.Cim = _cim;
+            this.Ar = _ar;
+        }
+
+        public void Kiiras()
+        {
+            Console.WriteLine($"A {Cim} című könvy ára {Ar} Forint.");
+        }
     }
 }
--- Konyv/Program.cs
+++ Konyv/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Konyv;
+
+Konyv.Konyv konvy1 = new Konyv.Konyv("The Uncanny", 4500);
+Konyv.Konyv konvy2 = new Konyv.Konyv("The Poetics of Space", 5270);
+
+konvy1.Kiiras();
+konvy2.Kiiras();
