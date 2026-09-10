7f0b800755ab084cbe6646038d1d71a3c8922367
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-10 11:13:57 +0200

    Bankszámla használata

--- Bankszamla/Program.cs
+++ Bankszamla/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using Bankszamla;
+
+Bankszamla.Bankszamla bankszamla = new Bankszamla.Bankszamla(true);
+
+bankszamla.Befizetés(3800);
+bankszamla.Befizetés(-103);
+bankszamla.Befizetés(12322);
+bankszamla.Kivet(211);
+bankszamla.Kivet(99999);
